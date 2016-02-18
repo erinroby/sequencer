91f45d9b3ea9dba4a83622f52e8a59d2de469f72
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-02-17 18:42:31 -0800

    add polygon object

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 var keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'a#', 'c#', 'd#', 'f#', 'g#'];
 var synth = new AudioSynth;
 var piano = synth.createInstrument('piano');
+var world = Physics();
 
-// create key object that does the following:
+function Polygon(x, y, radius, sides) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.sides = sides;
+  this.drawPolygon = function() {
+    var contx = document.getElementById("viewport").getContext("2d");
+    var angle = (Math.PI * 2) / this.sides;
+    contx.beginPath();
+    contx.translate(this.x, this.y);
+    for (var i = 0; i < this.sides; i++) {
+      contx.lineTo(this.radius * Math.cos(angle * i), this.radius * Math.sin(angle * i));
+    };
+    contx.closePath();
+    contx.stroke();
+  };
+  this.rotate = function() {
+
+  }
+}
+
+//Key object constructor that does the following:
 // have a note property.
 // have an event listener.
 // future: create new circle body with note property attached.
@@ -14,7 +36,7 @@ function Key(note, keyEl) {
   this.addListener = function() {
     this.keyEl.addEventListener("click", function(event) {
       event.preventDefault();
-      piano.play(that.note, 4, 2);
+      piano.play(that.note, 4, 2); // write an if statement for the octave 3 OR 4 here.
     });
   }
 }
@@ -25,3 +47,8 @@ for (var i = 0; i < keys.length; i++) {
   var key = new Key(keys[i].toUpperCase(), document.getElementById(keys[i] + 'Key'));
   key.addListener();
 };
+
+//create hexagon and render.
+
+var hexagon = new Polygon(200, 200, 100, 6);
+hexagon.drawPolygon();
