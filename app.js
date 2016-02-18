143094e8237349385d7a5ada0fa4cf87aa05ec53
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-02-17 20:26:02 -0800

    add ball object and event listener

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ var keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'a#', 'c#', 'd#', 'f#', 'g#'];
 var synth = new AudioSynth;
 var piano = synth.createInstrument('piano');
 var world = Physics();
+var contx = document.getElementById("viewport").getContext("2d");
 
-function Polygon(x, y, radius, sides) {
+function Polygon(x, y, radius, sides, color) {
   this.x = x;
   this.y = y;
   this.radius = radius;
   this.sides = sides;
-  this.drawPolygon = function() {
-    var contx = document.getElementById("viewport").getContext("2d");
+  this.color = color;
+  this.draw = function() {
     var angle = (Math.PI * 2) / this.sides;
     contx.beginPath();
     contx.translate(this.x, this.y);
@@ -17,11 +18,24 @@ function Polygon(x, y, radius, sides) {
       contx.lineTo(this.radius * Math.cos(angle * i), this.radius * Math.sin(angle * i));
     };
     contx.closePath();
+    contx.strokeStyle = this.color;
+    contx.lineWidth = 2;
     contx.stroke();
   };
-  this.rotate = function() {
+}
 
-  }
+function Ball(x, y, radius, color) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.color = color;
+  this.draw = function() {
+    contx.beginPath();
+    contx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+    contx.closePath();
+    contx.fillStyle = this.color;
+    contx.fill();
+  };
 }
 
 //Key object constructor that does the following:
@@ -36,19 +50,17 @@ function Key(note, keyEl) {
   this.addListener = function() {
     this.keyEl.addEventListener("click", function(event) {
       event.preventDefault();
-      piano.play(that.note, 4, 2); // write an if statement for the octave 3 OR 4 here.
+      var circle = new Ball(0, 0, 5, 'blue');
+      circle.draw();
+      piano.play(that.note, 4, 2); // write an if statement for the octave 3 OR 4 here and refactor to ball object.
     });
   }
 }
-
-// interate over the keys, create key objects.
 
 for (var i = 0; i < keys.length; i++) {
   var key = new Key(keys[i].toUpperCase(), document.getElementById(keys[i] + 'Key'));
   key.addListener();
 };
 
-//create hexagon and render.
-
-var hexagon = new Polygon(200, 200, 100, 6);
-hexagon.drawPolygon();
+var hexagon = new Polygon(250, 250, 100, 6, 'orange');
+hexagon.draw();
