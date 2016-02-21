90f2cd6261dc3f7f1cad13a0e415304aae4335e6
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-02-21 10:33:30 -0800

    connect keyboard

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,8 +1,6 @@
 var keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'a#', 'c#', 'd#', 'f#', 'g#'];
 var synth = new AudioSynth;
 var piano = synth.createInstrument('piano');
-var canvas = document.getElementById("viewport");
-var contx = canvas.getContext("2d");
 
 function Key(note, keyEl) {
   this.note = note;
@@ -11,7 +9,13 @@ function Key(note, keyEl) {
   this.addListener = function() {
     this.keyEl.addEventListener("click", function(event) {
       event.preventDefault();
-      window.requestAnimationFrame(draw);
+      World.add(world, Bodies.circle(300, 300, 10, {
+        restitution: 1,
+        friction: 0,
+        frictionAir: 0,
+        frictionStatic: 0,
+        force: { x: 0.01, y: 0.01}
+      }));
       piano.play(that.note, 4, 2); // write an if statement for the octave 3 OR 4 here and refactor to ball object. How to pass to new ball?
     });
   }
@@ -21,3 +25,5 @@ for (var i = 0; i < keys.length; i++) {
   var key = new Key(keys[i].toUpperCase(), document.getElementById(keys[i] + 'Key'));
   key.addListener();
 };
+
+// var aNewBodyElement = document.createElement("body");
--- js/physics.js
+++ js/physics.js
@@ -6,11 +6,14 @@ var Engine = Matter.Engine,
   Composites = Matter.Composites;
 
 // boilerplate: create a Matter.js engine
+// var canvasEl = document.getElementById('viewport');
+// document.body = canvasEl;
+
 var engine = Engine.create(document.body, {
   render: {
     options: {
-      wireframes: true,
-      showAngleIndicator: true
+      wireframes: false,
+      showAngleIndicator: false
     }
   }
 });
@@ -55,13 +58,13 @@ var stackBoundry = Composites.stack(100, 100, 1, 4, 0, 0, function(x, y) {
 World.add(world, stackBoundry);
 
 //add beach balls
- World.add(world, Bodies.circle(300, 300, 20, {
-   restitution: 1,
-   friction: 0,
-   frictionAir: 0,
-   frictionStatic: 0,
-   force: { x: 0.01, y: 0.01}
- }));
+ // World.add(world, Bodies.circle(300, 300, 10, {
+ //   restitution: 1,
+ //   friction: 0,
+ //   frictionAir: 0,
+ //   frictionStatic: 0,
+ //   force: { x: 0.01, y: 0.01}
+ // }));
 
 //add boundry rotation, basic animation timer boilerplate
 var angle = 0.01;
